b90d14e9dc63ae29e66ba3dd3738672bab66144e
Miguel Angel Garcia <miguel.garcia@gmail.com>
2021-06-11 12:03:04 -0300

    Extended the sender to generate random status data

--- MassRobotics-AMR-Sender/client.py
+++ MassRobotics-AMR-Sender/client.py
@@ -6,13 +6,17 @@ import json
 import time
 import uuid
 import hashlib
-from datetime import datetime
+import random
+import math
+import sys
+from datetime import datetime, timezone, timedelta
 
-async def sendMessage():
-  uri = "ws://localhost:3000"
+OPERATIONAL_STATES = ["navigating", "idle", "disabled", "offline", "charging", "waiting", "loadingUnloading", "manualOveride"] 
+
+async def sendMessage(uri):
   async with websockets.connect(uri) as websocket:
     # These could come from a configuration file or environment variables or any other source, but they are assumed not to change!
-    identity = {"manufacturerName": "Mass Robotics AMR", "robotModel": "AMR-01", "robotSerialNumber": "0000001", "baseRobotEnvelope": {"x": 0.5, "y": 1}}
+    identity = {"manufacturerName": "Mass Robotics AMRs", "robotModel": "AMR-01", "robotSerialNumber": "0000001", "baseRobotEnvelope": {"x": 0.5, "y": 1}}
 
     # Generate a uuid that will be consistent for this robot
     m = hashlib.md5()
@@ -29,14 +33,48 @@ async def sendMessage():
 
     status = {"uuid": str(uid)}
     while True:
-      status["timestamp"] = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S%z")
+      nowDt = datetime.now(timezone.utc)
+      now = nowDt.timestamp()
+      status["timestamp"] = nowDt.strftime("%Y-%m-%dT%H:%M:%S%z")
 
-      # The following information would need to be pulled out in a vendor-specific way, for this example it is just hard-coded
+      # The following information would need to be pulled out in a vendor-specific way,
+      # for this example it is just randomly generated
+      
       # Get the current operational state and put it in the status message
-      status["operationalState"] = "navigating" 
+      status["operationalState"] = random.choice(OPERATIONAL_STATES)
 
-      # Get the current location and put it in the status message
-      status["location"] = {"x": 0.5, "y": 1, "z": 0, "angle": {"w": 0.924, "x": 0.000, "y": 0.000, "z": 0.383}, "planarDatum": "4B8302DA-21AD-401F-AF45-1DFD956B80B5"}
+      # Generate a battery percentage that goes between 20 and 100
+      status["batteryPercentage"] = 20 + now % 81
+      
+      # Pick a random velocity
+      status["velocity"] = random.choice([
+        # deviating 10 degrees to the left
+        { "linear": 3, "angular": { "x": 0., "y": 0., "z": 0.087, "w": 0.996 } },
+        # deviating 15 degrees to the right
+        { "linear": 1, "angular": { "x": 0., "y": 0., "z": -0.131, "w": 0.991 } }
+      ])
+
+      # Generate a random location, simulating that the AMR moves in circles
+      r = 20
+      angle = random.choice([
+        { "x": 0., "y": 0., "z": 0.087, "w": 0.996 },
+        { "x": 0., "y": 0., "z": -0.131, "w": 0.991 }
+      ])
+      status["location"] = {"x":  math.cos(now) * r, "y": math.sin(now) * r, "z": 0, "angle": angle, "planarDatum": "4B8302DA-21AD-401F-AF45-1DFD956B80B5"}
+
+      # Generate random paths and destinations
+      side = random.choice([10, 20, 30])
+      status["path"] = [
+        {"timestamp": (nowDt+timedelta(seconds=10)).strftime("%Y-%m-%dT%H:%M:%S%z"), "x": 0, "y": 0, "angle": { "x": 0., "y": 0., "z": 0., "w": 1 }, "planarDatumUUID": "4B8302DA-21AD-401F-AF45-1DFD956B80B5"},
+        {"timestamp": (nowDt+timedelta(seconds=20)).strftime("%Y-%m-%dT%H:%M:%S%z"), "x": 0, "y": side, "angle": { "x": 0., "y": 0., "z": 0., "w": 1 }, "planarDatumUUID": "4B8302DA-21AD-401F-AF45-1DFD956B80B5"},
+        {"timestamp": (nowDt+timedelta(seconds=30)).strftime("%Y-%m-%dT%H:%M:%S%z"), "x": side, "y": side, "angle": { "x": 0., "y": 0., "z": 0., "w": 1 }, "planarDatumUUID": "4B8302DA-21AD-401F-AF45-1DFD956B80B5"},
+        {"timestamp": (nowDt+timedelta(seconds=40)).strftime("%Y-%m-%dT%H:%M:%S%z"), "x": side, "y": 0, "angle": { "x": 0., "y": 0., "z": 0., "w": 1 }, "planarDatumUUID": "4B8302DA-21AD-401F-AF45-1DFD956B80B5"},
+        {"timestamp": (nowDt+timedelta(seconds=50)).strftime("%Y-%m-%dT%H:%M:%S%z"), "x": 0, "y": 0, "angle": { "x": 0., "y": 0., "z": 0., "w": 1 }, "planarDatumUUID": "4B8302DA-21AD-401F-AF45-1DFD956B80B5"}
+      ]
+      status["destinations"] = [
+        {"timestamp": (nowDt+timedelta(seconds=30)).strftime("%Y-%m-%dT%H:%M:%S%z"), "x": side, "y": side, "angle": { "x": 0., "y": 0., "z": 0., "w": 1 }, "planarDatumUUID": "4B8302DA-21AD-401F-AF45-1DFD956B80B5"},
+        {"timestamp": (nowDt+timedelta(seconds=50)).strftime("%Y-%m-%dT%H:%M:%S%z"), "x": 0, "y": 0, "angle": { "x": 0., "y": 0., "z": 0., "w": 1 }, "planarDatumUUID": "4B8302DA-21AD-401F-AF45-1DFD956B80B5"}
+      ]
 
       # Send the report message
       await websocket.send(json.dumps(status))
@@ -44,4 +82,8 @@ async def sendMessage():
       # Wait for 1 second
       time.sleep(1)
 
-asyncio.get_event_loop().run_until_complete(sendMessage())
+uri = "ws://localhost:3000"
+if len(sys.argv) > 1:
+  # Read the receiver URI from the command line, if provided.
+  uri = sys.argv[1]
+asyncio.get_event_loop().run_until_complete(sendMessage(uri))
